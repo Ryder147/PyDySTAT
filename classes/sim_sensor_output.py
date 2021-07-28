edc4edc57d38545733c545929708f65c9e2711ab
lilartisto <orginalartis@gmail.com>
2021-07-28 11:08:58 +0200

    Sim_Sensor_Output class

---
+++ classes/sim_sensor_output.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+class Sim_Sensor_Output:
+    
+    def __init__(self, path):
+        self.sim_values = []
+        self.__setup_sim_values(path)
+        
+    def __setup_sim_values(self, path):
+        df = self.__read_dataframe(path)
+        
+        for i in range(0, df.shape[0]):
+            self.sim_values.append(df.iloc[i, 3])
+        
+    def __read_dataframe(self, path):
+        return pd.read_csv(
+            path, 
+            skiprows = 1,
+            header = 0,
+            sep = "\s*[;]\s*",
+            engine = 'python',
+            index_col=False)
+    
+path = r'C:\Users\orgin\Desktop\studia\praktyki\BTEX_NOWY\MODEL-A\ASP01_MODEL-A.txt'
+tmp = Sim_Sensor_Output(path)
